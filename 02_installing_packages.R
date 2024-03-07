5a3efddef2eebc950e91d5e0ebc0eeeebfd2dadb
ilagia <100956875+ilagia@users.noreply.github.com>
2024-03-07 17:48:46 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,5 @@
+#Installing new packages in R
+install.packages("terra") #funzione per installare pacchetti, dentro alle () ci metto il nome del paccheto, usiamo le virgolette per proteggerci
+library  #carica il pacchetto dentro R
+#errore rosso= warning, non molto gravi, da controllare ma r va comunque avanti. error=problema
+#funzione per rimuovere pacchetti: remove.packages()
