8715adfa5c62e57acaccc2c2010e5350248e39ad
ilagia <100956875+ilagia@users.noreply.github.com>
2024-03-18 08:16:43 +0100

    02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,5 +1,14 @@
 #Installing new packages in R
 install.packages("terra") #funzione per installare pacchetti, dentro alle () ci metto il nome del paccheto, usiamo le virgolette per proteggerci
-library  #carica il pacchetto dentro R
+library()  #carica il pacchetto dentro R  # or require()
 #errore rosso= warning, non molto gravi, da controllare ma r va comunque avanti. error=problema
 #funzione per rimuovere pacchetti: remove.packages()
+
+# install.packages("devtools")
+library(devtools)
+
+# install the imageRy package from GitHub
+# a devtools function:
+# install_github("ducciorocchini/imageRy")
+
+library(imageRy)
