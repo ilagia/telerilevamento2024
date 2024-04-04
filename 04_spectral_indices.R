475b5ded5d1e73da89e630d0c1b9e7cebbf6f7b0
ilagia <100956875+ilagia@users.noreply.github.com>
2024-04-04 17:34:34 +0200

    Update 04_spectral_indices.R

--- 04_spectral_indices.R
+++ 04_spectral_indices.R
@@ -5,7 +5,7 @@ library(imageRy)
 library(terra)
 
 #list of files
-im.list
+im.list()
 #importing data
 m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
 # band 1 =nir
@@ -38,6 +38,40 @@ im.plotRGB(m1992, 2, 3, 1) # nir on B 1992
 im.plotRGB(m2006, 1, 2, 3) # nir on R 2006
 im.plotRGB(m2006, 2, 1, 3) # nir on G 2006
 im.plotRGB(m2006, 2, 3, 1) # nir on B 2006
+
+#-----
+
+#Calculating the DVI (Difference Vegetation Index)
+#sottrazione fra bit della banda del nir e quella del rosso = dvi
+#valori vanno da 255 a -255, dipende dalla risoluzione radiometrica (n. bit)
+# band 1 =nir
+# band 2 = red
+# band 3 = green
+
+dvi1992 = m1992[[1]] - m1992[[2]]
+#posso anche fare con $
+
+cl <- colorRampPalette(c("dark blue", "yellow", "red", "black"))(100) # specifying a color scheme
+plot(dvi1992, col=cl)
+
+#dvi 2006
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+dvi2006 = m2006[[1]] - m2006[[2]]
+plot(dvi2006, col=cl)
+#plottiamo le immagini insieme
+par(mfrow=c(1,2))
+plot(dvi1992, col=cl)
+plot(dvi2006, col=cl)
+
+#indice nDVI: serve per comparare 2 immagini che hanno un numero diverso di bit
+ndvi1992 = dvi1992 / (m1992[[1]] + m1992[[2]])
+ndvi2006 = dvi2006 / (m2006[[1]] + m2006[[2]])
+
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+
 
 
 
