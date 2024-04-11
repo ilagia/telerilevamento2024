733cec48c1981644582b7a3622daa3e15e240c29
ilagia <100956875+ilagia@users.noreply.github.com>
2024-04-11 17:34:47 +0200

    Update 06_time_siries.R

--- 06_time_siries.R
+++ 06_time_siries.R
@@ -1,12 +1,12 @@
-#Second method to quantify changes in time
-#first method was based on classification
+# Second method to quantify changes in time
+# First method was based on classification
 
 library(imageRy)
 library(terra)
 
 im.list()
 
-#import the data #prendo il primo e l'ultimo
+# import the data #prendo il primo e l'ultimo
 
 EN01 <- im.import("EN_01.png")
 EN13 <- im.import("EN_13.png")
@@ -18,6 +18,41 @@ im.plotRGB.auto(EN13)
 difEN = EN01[[1]] - EN13[[1]]
 cl <- colorRampPalette(c("blue", "white","red")) (100)
 plot(difEN, col=cl)
+
+
+## ice melt in Greenland
+
+im.list()
+g2000 <- im.import("greenland.2000.tif")
+clg <- colorRampPalette(c("black","blue","white","red")) (100)
+plot(g2000, col=clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+par(mfrow=c(1,2))
+plot(g2000, col=clg)
+plot(g2015, col=clg)
+
+par(mfrow=c(2,2))
+plot(g2000, col=clg)
+plot(g2005, col=clg)
+plot(g2010, col=clg)
+plot(g2015, col=clg)
+
+# or stacking the data
+greenland <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
+
+difg = greenland[[1]] - greenland[[4]]
+clgreen <- colorRampPalette(c("red", "white", "blue")) (100)
+plot(difg, col=clgreen)
+
+im.plotRGB(greenland, r=1, g=2, b=4) # g2000 on red, g2005 on green, g2015 on blue, tutte le parti blu hanno temp più alte nel 2015 le rosse nel 2000 e così via
+
+
+
 
 
 
